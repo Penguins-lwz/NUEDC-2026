51ca6ddef8695cad180f54d3bab615df898ff297
Penguins-lwz <hust.penguins@qq.com>
2025-10-12 23:10:43 +0800

    Optimize bsp_uart.c
    - remove redundant checks

--- 251009_Penguins_G474_Demo/BSP/Src/bsp_uart.c
+++ 251009_Penguins_G474_Demo/BSP/Src/bsp_uart.c
@@ -11,8 +11,7 @@ void BSP_UART_Transmit_DMA(BSP_UART_HandleTypeDef *buart, const char *format, ..
 {
 	va_list ap;
 	va_start(ap, format);
-	while (buart->huart->gState != HAL_UART_STATE_READY \
-		|| buart->huart->hdmatx->State != HAL_DMA_STATE_READY);
+	while (buart->huart->gState != HAL_UART_STATE_READY);
 	vsnprintf(buart->pTxMsg, UART_BUF_SIZE, format, ap);
 	va_end(ap);
 	HAL_UART_Transmit_DMA(buart->huart, (uint8_t *)buart->pTxMsg, strlen(buart->pTxMsg));
