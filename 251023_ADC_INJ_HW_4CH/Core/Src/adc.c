62604edcaf713272e421d5eba8ae80ab3994ac69
Penguins-lwz <hust.penguins@qq.com>
2025-10-26 14:57:55 +0800

    Fix: correct misconfigured EOC selection

--- 251023_ADC_INJ_HW_4CH/Core/Src/adc.c
+++ 251023_ADC_INJ_HW_4CH/Core/Src/adc.c
@@ -49,7 +49,7 @@ void MX_ADC1_Init(void)
   hadc1.Init.DataAlign = ADC_DATAALIGN_RIGHT;
   hadc1.Init.GainCompensation = 0;
   hadc1.Init.ScanConvMode = ADC_SCAN_ENABLE;
-  hadc1.Init.EOCSelection = ADC_EOC_SINGLE_CONV;
+  hadc1.Init.EOCSelection = ADC_EOC_SEQ_CONV;
   hadc1.Init.LowPowerAutoWait = DISABLE;
   hadc1.Init.ContinuousConvMode = DISABLE;
   hadc1.Init.NbrOfConversion = 1;
